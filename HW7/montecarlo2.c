6f2096f7d8aa54e5180b71075ed2e8ddbd55721c
Wenyue Wang (Summer) <wwy305756464@gmail.com>
2020-11-16 21:43:12 -0500

    add

--- HW7/montecarlo2.c
+++ HW7/montecarlo2.c
@@ -5,37 +5,50 @@
 #include <omp.h>
 
 int main(int argc, char* argv[]) {
-    long long total = 1e6;  // 默认100万个样本
-    int tn = 2;             // 默认2个线程
-    if(argc >= 2) {
-        total = atoi(argv[1]);  // 从参数中获取样本数
-    }
-    if(argc >= 3) {
-        tn = atoi(argv[2]);     // 从参数中获得线程数
-    }
+    long long total = atoi(argv[1]);
+    int tn = atoi(argv[2]);
 
-    long long count = 0;
-    double x, y;
-    #pragma omp parallel num_threads(tn)
-    {
+
+    // if(argc >= 2) {
+    //     total = atoi(argv[1]);  
+    // }
+    // if(argc >= 3) {
+    //     tn = atoi(argv[2]); 
+    // }
+
+    long count = 0;
+    long total = 0;
+    double x, y, y_check;
+    double y_max = 5.4365636;
+
+    #pragma omp parallel num_threads(tn){
         unsigned seed = time(NULL);
 
         #pragma omp for private(x, y) reduction(+:count)
+
         for(long long i = 0; i < total; i++) {
             x = (double)rand_r(&seed) / RAND_MAX;
-            y = (double)rand_r(&seed) / RAND_MAX;
-            if(x*x + y*y <= 1) {
+            y = (double)rand_r(&seed) / RAND_MAX * y_max;
+            y_check = 2 * x * exp(x * x);
+            total++;
+            
+            if(y <= y_check) {
                 count++;
             }
         }
     }
-    double pi = 4 * (double)count / total;
 
-    printf("[+] total = %lld\n", total);
-    printf("[+] count = %lld\n", count);
-    printf("[+] pi    = %f\n", pi);
-    printf("[+] loss  = %e\n", acos(-1) - pi);
-    printf("\nnum_threads = %d\n", tn);
+    double e_true = exp(1)
+    double e = (double)count / total * y_max + 1;
 
+    // printf("[+] total = %lld\n", total);
+    // printf("[+] count = %lld\n", count);
+    // printf("[+] e    = %f\n", e);
+    // printf("num_threads = %d\n", tn);
+
+    printf("number of threads: %d\n", tn);
+    printf("number of points: %.0f\n", total);
+    printf("approximate e: %f\n", e);
+    printf("theoretical e: %f\n", e_true);
     return 0;
 }
