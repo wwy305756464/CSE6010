5d0f7097156f1568c323fd6fc4e1f051e05815f3
Wenyue Wang (Summer) <wwy305756464@gmail.com>
2020-11-16 22:02:03 -0500

    add

--- HW7/montecarlo2.c
+++ HW7/montecarlo2.c
@@ -5,50 +5,41 @@
 #include <omp.h>
 
 int main(int argc, char* argv[]) {
+    // define number of points and number of threads 
     long long N = atoi(argv[1]);
-    int tn = atoi(argv[2]);
+    int threads = atoi(argv[2]);
 
-
-    // if(argc >= 2) {
-    //     total = atoi(argv[1]);  
-    // }
-    // if(argc >= 3) {
-    //     tn = atoi(argv[2]); 
-    // }
-
+    // for summarize how many points under the curve
     long count = 0;
-    long total = 0;
     double x, y, y_check;
     double y_max = 5.4365636;
 
-    #pragma omp parallel num_threads(tn)
+    #pragma omp parallel num_threads(threads)
     {
-        unsigned seed = time(NULL);
+        unsigned seed = time(NULL); // randome seed
 
         #pragma omp for private(x, y) reduction(+:count)
 
+        // calculate whether the point under the curve or not
         for(long long i = 0; i < N; i++) {
             x = (double)rand_r(&seed) / RAND_MAX;
             y = (double)rand_r(&seed) / RAND_MAX * y_max;
             y_check = 2 * x * exp(x * x);
-            total++;
             
+            // if under the curve
             if(y <= y_check) {
                 count++;
             }
         }
     }
 
+    // calculate e
     double e_true = exp(1);
     double e = (double)count / N * y_max + 1;
 
-    // printf("[+] total = %lld\n", total);
-    // printf("[+] count = %lld\n", count);
-    // printf("[+] e    = %f\n", e);
-    // printf("num_threads = %d\n", tn);
-
-    printf("number of threads: %d\n", tn);
-    printf("number of points: %.0f\n", N);
+    // print out
+    printf("number of threads: %d\n", threads);
+    printf("number of points: %d\n", N);
     printf("approximate e: %f\n", e);
     printf("theoretical e: %f\n", e_true);
     return 0;
